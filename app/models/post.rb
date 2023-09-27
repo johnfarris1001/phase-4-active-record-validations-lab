4a219fd2bc60699bc005ff914a3029877f890a72
John <johnfarris217@gmail.com>
2023-09-27 15:27:41 -0500

    commit tree 761716a88b5e725b0977d3156e179f3c3f70edbb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :title_must_be_clickbaity
+
+    def title_must_be_clickbaity
+        if title
+            if !["Won't Believe", "Secret", "Top", "Guess"].any? { |w| title.include?(w) }
+                errors.add(:title, "must be clickbait-y")
+            end
+        end
+    end
 end
